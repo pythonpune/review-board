3defd6bad4edb7b56fb50ae4dad208415539bd8a
Sopan Shewale <sopan.shewale@gmail.com>
2017-10-29 14:55:10 +0530

    Enabling plugin  - calling method from plugin code

--- app.py
+++ app.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 import yaml
 import click
+from  plugins.pagure import  get_pull_requests
+
 
 @click.command()
 @click.option('--site', default='config', help='Please enter the sitename to list activity, e.g. gitgub')
 @click.option('--username', default='config', help='Please enter username to list activity, e.g. HariSadu')
-def execute_board(site,  username):
+@click.option('--repo', default='config', help='Please enter repository name to list activity, e.g. helloworld')
+def execute_board(site,  username, repo):
     '''Take Arguments from command line OR Read configuration files, which is YAML format'''
     #print (site)
     #print (username) 
-    if site == 'config'  and username == 'config':
+    if site == 'config'  and username == 'config' and repo == 'config':
           print ("The Application will read username and code sites from configuraiton file")
           with open('config.yml', 'r') as f:
              conf = yaml.load(f)
+
+    # Expect site, username and repo comming from command line 
+    #data = get_pull_requests('walters', 'fedora-atomic')
+    data = get_pull_requests(username, repo)
+    print (data)
    
+
 
 
 def getComments(site, username):
