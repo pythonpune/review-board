611faa0002bcb2bbd7f77525899636850b008621
Vikas Mulaje <vmulaje@redhat.com>
2017-10-29 15:33:41 +0530

    added github.py module which will extract all the title fo current opend pr's

---
+++ plugins/github.py
@@ -0,0 +1,13 @@
+import requests
+import json
+list_of_pull_requests=[]
+def get_pull_requests(username,repository):
+    r=requests.get("https://api.github.com/repos/"+username+"/"+repository+"/pulls")
+    js=json.loads(r.content)
+    for i in range(0,len(js)):
+        if 'title' in  js[i].keys():
+            list_of_pull_requests.append(js[i]['title'])
+    return list_of_pull_requests
+
+
+#print get_pull_requests(username,repository)
