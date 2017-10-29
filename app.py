dafd5be90f5f60e17ed4d3c2aae94c6ed4ddf7b2
Sopan Shewale <sopan.shewale@gmail.com>
2017-10-29 14:44:55 +0530

    changed the name for command line option subroutine

--- app.py
+++ app.py
@@ -3,13 +3,27 @@ import yaml
 import click
 
 @click.command()
-@click.option('--site', default='github', help='Please enter the sitename to list activity, e.g. gitgub')
-@click.option('--username', default='harisadu', help='Please enter username to list activity, e.g. HariSadu')
-def cmd_options(site,  username):
-    print (site)
-    print (username) 
+@click.option('--site', default='config', help='Please enter the sitename to list activity, e.g. gitgub')
+@click.option('--username', default='config', help='Please enter username to list activity, e.g. HariSadu')
+def execute_board(site,  username):
+    '''Take Arguments from command line OR Read configuration files, which is YAML format'''
+    #print (site)
+    #print (username) 
+    if site == 'config'  and username == 'config':
+          print ("The Application will read username and code sites from configuraiton file")
+          with open('config.yml', 'r') as f:
+             conf = yaml.load(f)
+   
+
+
+def getComments(site, username):
+     pass
+
+def getPR(site, username):
+     pass 
 
 
 if __name__ == '__main__':
-    print ("Welcome to Code Review System")
-    cmd_options()
+    click.echo("Welcome to Code Review System\n")
+    execute_board()
+
