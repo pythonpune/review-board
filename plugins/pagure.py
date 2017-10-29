b860f82f1d8168efb8f4b94f7753e66991f566b7
Bhavin Gandhi <bhavin7392@gmail.com>
2017-10-29 14:34:37 +0530

    Add get_pull_requests for Pagure
    Signed-off-by: Bhavin Gandhi <bhavin7392@gmail.com>

---
+++ plugins/pagure.py
@@ -0,0 +1,19 @@
+from libpagure import Pagure
+
+def get_pull_requests(username, repository_name):
+    """
+    Returns the list of PR titles for specified username and repo from Pagure
+    :param username: username of the user on Pagure
+    :param repository_name: name of the repo on Pagure
+    :returns: List of titles of PRs
+    """
+    pg = Pagure()
+    pg.repo= repository_name
+    list_of_pull_requests = []
+    for pull_request in pg.list_requests():
+        if pull_request["user"]["name"] == username:
+            list_of_pull_requests.append(pull_request["title"])
+    
+    return list_of_pull_requests
+
+# print(get_pull_requests('walters', 'fedora-atomic'))
