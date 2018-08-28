0da4429cce0a02ef400205a93c8b54cdcf6a9048
ganeshhubale <ganeshhubale03@gmail.com>
2018-08-29 01:03:48 +0530

    List all open PRs made by particular user to particular repo. on GitHub
    Signed-off-by: ganeshhubale <ganeshhubale03@gmail.com>

--- plugins/github.py
+++ plugins/github.py
@@ -7,9 +7,10 @@ list_of_pull_requests=[]
 
 def get_pull_requests(username,repository):
     github_url = "https://api.github.com"
-    r = requests.get(os.path.join(github_url, "repos", username, repository, "pulls"))
+    r = requests.get(os.path.join(github_url, "repos", repository, "pulls"))
     js = json.loads(r.content)
     for i in range(0, len(js)):
-        if 'title' in js[i].keys():
-            list_of_pull_requests.append(js[i]['title'])
+        if username == js[i]['user']['login']:
+            if 'title' in js[i].keys():
+                list_of_pull_requests.append(js[i]['title'])
     return list_of_pull_requests
