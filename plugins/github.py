ecb5139616f954c56d0aa5563f5e59b36ce21bf3
ganeshhubale <ganeshhubale03@gmail.com>
2018-08-29 01:13:47 +0530

    Plugin for GitHub
    Signed-off-by: ganeshhubale <ganeshhubale03@gmail.com>

--- plugins/github.py
+++ plugins/github.py
@@ -3,14 +3,16 @@ import json
 import os
 
 
-list_of_pull_requests=[]
+list_of_pull_requests = []
 
-def get_pull_requests(username,repository):
+
+def get_pull_requests(username, repository):
+
     github_url = "https://api.github.com"
     r = requests.get(os.path.join(github_url, "repos", repository, "pulls"))
     js = json.loads(r.content)
-    for i in range(0, len(js)):
-        if username == js[i]['user']['login']:
-            if 'title' in js[i].keys():
-                list_of_pull_requests.append(js[i]['title'])
+    for pull in js:
+        if username == pull['user']['login']:
+            if 'title' in pull.keys():
+                list_of_pull_requests.append(pull['title'])
     return list_of_pull_requests
